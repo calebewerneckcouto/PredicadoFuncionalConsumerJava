3508aa7f1a7d54698fba96305ee3951550f000d4
calebewerneckcouto <calebewerneck@hotmail.com>
2024-12-07 13:05:54 -0300

    expressao lambda declarada

--- src/application/Program.java
+++ src/application/Program.java
@@ -3,6 +3,7 @@ package application;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Consumer;
 
 import entities.Product;
 
@@ -18,10 +19,13 @@ public class Program {
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
 
-	
-        list.forEach(Product::nonStaticPriceUpdate);
-        
-       list.forEach(System.out::println);
-		
+		Consumer<Product> cons = c -> {
+			c.setPrice(c.getPrice() * 1.1);
+		};
+
+		list.forEach(cons);
+
+		list.forEach(System.out::println);
+
 	}
 }
